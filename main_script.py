99a157f9514187d334cbc6ee9146585cb4fa667c
bobobo34 <evinlodder@gmail.com>
2022-04-12 18:16:54 -0400

    start

--- main_script.py
+++ main_script.py
@@ -1,1 +1,11 @@
-print("fart")
+from mysql.connector import connect, Error
+
+try:
+    with connect(
+        host="localhost",
+        user=input("Enter username: "),
+        password=input("Enter password: "),
+    ) as connection:
+        print(connection)
+except Error as e:
+    print(e)
